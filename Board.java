41ac09b6632a7be8d08b8183353ea0428a59d593
AHS CS <ahs-cs@ausdk12.org>
2025-05-30 12:28:24 -0700

    save

--- Board.java
+++ Board.java
@@ -6,7 +6,7 @@ public class Board  {
     private int cols;
     
     /** The grid of pieces */
-    private Player[][] grid;
+    private Player[][] grid; //(0,0) bottom left (0,7)
     
     
 
@@ -51,28 +51,83 @@ public class Board  {
     
     //returns true if there are no more plays left
     public boolean boardFilled(){
-        //TODO: write this
-        return false; 
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if(grid[r][c] == null)
+                    return false;
+            }
+        }
+        return true;
     }
 
     // Returns true if move is possible given board state.  
     public boolean possibleMove(Move move) {
-        // TODO: write this.  Right now, it ignores filled columns, claiming any move is possible
-        return true;
+        for (int r = 0; r < rows; r++) {
+            if (grid[r][move.getColumn()]==null)
+                return true;
+        }
+        return false;
     }
     
     // Adds a piece to the board for a given Move
     public void addPiece(Move move) {
-        //TODO: this is a test stub, you need to rewrite this.
-    	grid[0][move.getColumn()] = move.getPlayer();
+        if (possibleMove(move)){
+            for (int r = 0; r < rows; r++) {
+                if (grid[r][move.getColumn()] == null){
+                    grid[r][move.getColumn()] = move.getPlayer();
+                    r += rows;
+                }
+            }
+        }
+    }
+
+    public boolean rowWinner(Move move){
+        int count = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c]==move.getPlayer()){
+                    count ++;
+                    if(count == 4)
+                        return true;
+                }
+                else
+                    count = 0;
+            }
+        }
+        return false;
+    }
+
+    public boolean colWinner(Move move){
+        int count = 0;
+        for (int r = 0; r < rows; r++) {
+            if(grid[r][move.getColumn()] == move.getPlayer()){
+                count ++;
+                if (count == 4)
+                    return true;
+            }
+            else   
+                count = 0;
+        }
+        return false;
+    }
+
+    public boolean diagonalWinner(Move move){
+        for(int r = 0; r < rows; r++){
+            if(grid[r][move.getColumn()] == move.getPlayer()){
+                
+            }
+        }
+
+        move.getPlayer();
     }
 
     // if the board contains a winning position, returns the Player that wins.
     // Otherwise, returns null.  You could ignore lastMove.
     public Player winner(Move lastMove) {
-        // TODO: write this.  Currently, there is never a winnder.
-        return null;
+        if (colWinner(lastMove)||rowWinner(lastMove))//||diagonalWinner(lastMove))
+            return lastMove.getPlayer();
+        else
+            return null;
     }
-    
     
 } // end Board class
